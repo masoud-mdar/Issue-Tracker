268bda509c778925d692ed76268cf4253d250e4b
Masoud <masoud_mdar@yahoo.fr>
2021-05-16 12:04:37 +0200

    Log.js modified

--- client/src/components/Log.js
+++ client/src/components/Log.js
@@ -1,16 +1,10 @@
 import React from "react"
 
 const Log = (props) => {
-    //const project = props.data.selectedProject
+
     const length = props.data.issuesList.length
-    console.log("in Log")
-    console.log(props.data.issuesList)
-    //console.log(length)
-
-
     const closedStyle = {"backgroundColor": "#00ff93"}
     const openStyle = {"backgroundColor": "orange"}
-
     const activeStyle = {"backgroundColor": "#002433"}
     const deactiveStyle = {"backgroundColor": ""}
 
@@ -35,25 +29,20 @@ const Log = (props) => {
                             props.data.issuesList.map((issue, n) => {
                                 return (
                                     <li key={Math.random() * Math.random()} style={issue.open ? openStyle : closedStyle}>
+                                        
                                         <div><p>{n+1}</p></div>
                                         <div><p>{issue._id}</p></div>
                                         <div><p>{issue.issue_title}</p></div>
                                         <div>{issue.updated_on ? <p>{issue.updated_on} </p> : <p>---</p>}</div>
                                         <div><p style={{"color": issue.open ? "crimson" : "#002433" }}>  {issue.open ? "open" : issue.issue_title === "First commit" ? "---" : "close"}</p></div>
                                         
-                                        
-                                        
-                                        
                                         <button name="details" id={issue._id} onClick={props.data.handleClick}>More details</button>
                                     </li>
                                 )
                             })
                         ) : (
-                            
                             !length && <li><h2>No Ticket found for this user...</h2></li>
-                            
                         )
-
                     }
                 </ul>
             </div>
