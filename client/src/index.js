19bd26b360a2aca9d0aba1b70f1ca9abb7bc7a5f
Masoud <masoud_mdar@yahoo.fr>
2021-09-03 12:19:00 +0200

    info part responsive

--- client/src/index.js
+++ client/src/index.js
@@ -6,5 +6,6 @@ import App from "./components/App"
 import "./styles/styles.css"
 import "./styles/navbar.css"
 import "./styles/allProjects.css"
+import "./styles/infoPart.css"
 
 ReactDOM.render(<App />, document.getElementById("root"))
