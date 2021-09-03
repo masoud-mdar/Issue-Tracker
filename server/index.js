f879045090d62e684bea021c2d3f0830e9828508
Masoud <masoud_mdar@yahoo.fr>
2021-09-03 13:13:37 +0200

    deploy configs

--- server/index.js
+++ server/index.js
@@ -14,8 +14,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
-//app.use(cors({origin: "https://issue-tracker-masoud.netlify.app", credentials: true}))
-app.use(cors({origin: "*", credentials: true}))
+app.use(cors({origin: "https://issue-tracker-masoud.netlify.app", credentials: true}))
 
 app.use(session({
     secret: process.env.SESSION_SECRET,
