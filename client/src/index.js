c718ab0a49d88540c0194fa51b7db123aec12b72
Masoud <masoud_mdar@yahoo.fr>
2021-09-03 12:55:16 +0200

    update issue responsive

--- client/src/index.js
+++ client/src/index.js
@@ -7,5 +7,7 @@ import "./styles/styles.css"
 import "./styles/navbar.css"
 import "./styles/allProjects.css"
 import "./styles/infoPart.css"
+import "./styles/moreInfo.css"
+import "./styles/updateIssue.css"
 
 ReactDOM.render(<App />, document.getElementById("root"))
